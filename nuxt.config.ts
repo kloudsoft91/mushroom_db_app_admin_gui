802a34616031ad259ee5c225a8ad7ff5a2b527c0
Karan Nayak <nayakaran@gmail.com>
2023-09-24 21:13:12 +1300

    Updated with renamed repo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default {
   // Configuration options here
   app: {
-    baseURL: '/', // baseURL: '/<repository>/'
+    baseURL: '/mushroom_db_app_admin_gui/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
   devtools: { enabled: true },
