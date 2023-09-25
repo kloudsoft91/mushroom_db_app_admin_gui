2b193538d854cbcba077fc7e7031d5a1f69a2a75
Karan Nayak <nayakaran@gmail.com>
2023-09-25 14:03:38 +1300

    Updated pipeline to use gh-pages module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
   // Configuration options here
-  modules: [
-    '@nuxt/ui',
-    'bootstrap-vue/nuxt'
-  ],
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    'faker-nuxt',
-    ['@nuxtjs/fontawesome', { icons: { solid: ['faHome'] } }]
-  ],
-  target: 'static',
-  router: {
-    base: '/mushroom_db_app_admin_gui/'
+  app: {
+    baseURL: '/mushroom_db_app_admin_gui/', // baseURL: '/<repository_name>/'
+    buildAssetsDir: 'assets',
   },
-  components: true,
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/ui'
+  ],
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: 'pages/index.vue'
+    },
+  ],
   //Tailwind CSS config
   TailwindModules: [
     '@nuxtjs/tailwindcss'
-  ],
-  build: {}
+  ]
 }
